1f1fee95777825a0aafea3ea4e48559362d4bb5a
Vadim Isakov <43072246+Iaotle@users.noreply.github.com>
2022-03-31 00:00:12 +0200

    syntax changes

--- server.py
+++ server.py
@@ -17,21 +17,21 @@ userlist = []
 def clientthread(conn, addr):
     # Login:
     if len(iplist) > 64:
-        conn.send("BUSY\n")
+        conn.send("NOTNOW\n")
         remove(conn)
     message = conn.recv(2048)
-    if message.startswith("HELLO-FROM"):
-        username = message.lstrip("HELLO-FROM ").rstrip("\n")
+    if message.startswith("BYE-TO"):
+        username = message.lstrip("BYE-TO ").rstrip("\n")
         if "\n" in username or " " in username:
-            conn.send("BAD-RQST-BODY\n")
+            conn.send("UR-FAT\n")
         elif username not in userlist:
             userlist.append(username)
-            conn.send("HELLO " + username + "\n")
+            conn.send("BYE " + username + "\n")
         else:
-            conn.send("IN-USE\n")
+            conn.send("IOU\n")
             remove(conn)
     else:
-        conn.send("BAD-RQST-HDR\n")
+        conn.send("BAD-HEAD\n")
         remove(conn)
 
     print(username + " connected.")
@@ -47,26 +47,26 @@ def clientthread(conn, addr):
                         message += conn.recv(4096)
                         index = message.find("\n")
                     sys.stdout.write(username + ": " + message[:index + 1])
-                    if message.startswith("SEND"):
+                    if message.startswith("RE"):
                         send = message[:index + 1].lstrip("SEND ").split(" ", 1)
                         if send[0] in userlist:
                             userindex = userlist.index(send[0])
                             if "\n" in send[1][:-1]:
-                                conn.send("BAD-RQST-BODY\n")
+                                conn.send("UR-FAT\n")
                             else:
-                                send = "DELIVERY " + username + " " + send[1] + "\n"
-                                conn.send("SEND-OK\n")
+                                send = "AMAZON " + username + " " + send[1] + "\n"
+                                conn.send("RECEIVE-OK\n")
                                 # send = "DELIVERY iaotle testmessage"
                                 iplist[userindex].send(send)
                                 # iplist[userindex].send(" done\nSEND-OK\nDELIVERY iaotle testmessage\n")
                                 message = message[index + 1:]
                         else:
                             conn.send("UNKNOWN\n")
-                    elif message.startswith("WHO"):
-                        conn.send("WHO-OK " + ", ".join(userlist) + "\n")
+                    elif message.startswith("WHODIS"):
+                        conn.send("DISME " + ", ".join(userlist) + "\n")
                         message = message[index + 1:]
                     else:
-                        conn.send("BAD-RQST-HDR\n")
+                        conn.send("BAD-HEAD\n")
                         message = message[index + 1:]
 
             else:
